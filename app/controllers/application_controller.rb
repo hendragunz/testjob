0c6712fae568508d0cc742002592b3ed2eb39a68
Hendra Gunawan <hendragunz@codecampz.com>
2014-09-20 03:16:34 +0700

    subdomain checking

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :check_domain
   before_action :configure_permitted_parameters, if: :devise_controller?
   layout :determine_template
 
@@ -10,9 +11,31 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:subdomain, :user_type, :email, :password, :password_confirmation) }
   end
 
+  def after_sign_in_path_for(resource)
+    root_url(subdomain: current_user.subdomain)
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_url(subdomain: nil)
+  end
+
   private
 
   def determine_template
     request.xhr? ? 'ajax' : 'application'
+  end
+
+  def check_domain
+    subdomain = request.subdomain
+
+    if user_signed_in?
+      if subdomain.present? && (subdomain != 'www') && (subdomain != current_user.subdomain)
+        return redirect_to root_url(subdomain: current_user.subdomain)
+      end
+    else
+      if (subdomain != 'www') && subdomain.present?
+        return redirect_to root_url(subdomain: '')
+      end
+    end
   end
 end
